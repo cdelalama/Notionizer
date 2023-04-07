b32b221843e6053440954eed0eb46096adb68d83
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-04-07 22:35:57 +0200

    problem with audio being too long

--- src/whisper.js
+++ src/whisper.js
@@ -13,21 +13,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.transcribe = void 0;
+// whisper.ts
 const axios_1 = __importDefault(require("axios"));
 const fs_1 = require("fs");
 const form_data_1 = __importDefault(require("form-data"));
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 function transcribe(audioFile) {
     return __awaiter(this, void 0, void 0, function* () {
-        const formData = new form_data_1.default();
-        formData.append('audio_file', (0, fs_1.createReadStream)(audioFile));
-        const response = yield axios_1.default.post('https://api.openai.com/v1/whisper/transcribe', formData, {
-            headers: {
-                'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
-                Authorization: `Bearer ${OPENAI_API_KEY}`,
-            },
-        });
-        return response.data.transcript;
+        try {
+            const formData = new form_data_1.default();
+            formData.append('audio_file', (0, fs_1.createReadStream)(audioFile));
+            const response = yield axios_1.default.post('https://api.openai.com/v1/whisper/transcribe', formData, {
+                headers: {
+                    'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
+                    Authorization: `Bearer ${OPENAI_API_KEY}`,
+                },
+            });
+            return response.data.transcript;
+        }
+        catch (error) {
+            console.error('Error in transcribe function:', error);
+            throw error;
+        }
     });
 }
 exports.transcribe = transcribe;
--- src/whisper.ts
+++ src/whisper.ts
@@ -1,3 +1,4 @@
+// whisper.ts
 import axios from 'axios';
 import { createReadStream } from 'fs';
 import FormData from 'form-data';
@@ -5,19 +6,24 @@ import FormData from 'form-data';
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 export async function transcribe(audioFile: string): Promise<string> {
-  const formData = new FormData();
-  formData.append('audio_file', createReadStream(audioFile));
+  try {
+    const formData = new FormData();
+    formData.append('audio_file', createReadStream(audioFile));
 
-  const response = await axios.post(
-    'https://api.openai.com/v1/whisper/transcribe',
-    formData,
-    {
-      headers: {
-        'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
-        Authorization: `Bearer ${OPENAI_API_KEY}`,
-      },
-    }
-  );
+    const response = await axios.post(
+      'https://api.openai.com/v1/whisper/transcribe',
+      formData,
+      {
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
+          Authorization: `Bearer ${OPENAI_API_KEY}`,
+        },
+      }
+    );
 
-  return response.data.transcript;
+    return response.data.transcript;
+  } catch (error) {
+    console.error('Error in transcribe function:', error);
+    throw error;
+  }
 }
