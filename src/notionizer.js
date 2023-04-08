f5255df456fa1462e5cc8cd5eac0a0db30060058
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-04-08 20:34:46 +0200

    separate audio in chunks

--- src/notionizer.js
+++ src/notionizer.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,58 +8,75 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// main.ts
 const grammy_1 = require("grammy");
 const fs_1 = require("fs");
-const whisper_1 = require("./whisper"); // Import your Whisper API wrapper here
-const dotenv = __importStar(require("dotenv"));
-const play_dl_1 = require("play-dl");
 const stream_1 = require("stream");
-dotenv.config();
-const bot = new grammy_1.Bot(process.env.BOT_TOKEN);
-bot.command('start', (ctx) => ctx.reply('Welcome! Send me a YouTube URL and I will transcribe the video for you.'));
-bot.on('message', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    if (ctx.message.text) {
-        const url = ctx.message.text;
-        console.log('URL received:', url);
+const ytdl_core_1 = __importDefault(require("ytdl-core"));
+const fluent_ffmpeg_1 = __importDefault(require("fluent-ffmpeg"));
+const whisper_1 = require("./whisper");
+const BOT_TOKEN = 'YOUR_BOT_TOKEN';
+const bot = new grammy_1.Bot(BOT_TOKEN);
+function splitAudioFile(inputFile, duration, chunkDuration) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const chunkCount = Math.ceil(duration / chunkDuration);
+        const outputFiles = [];
+        for (let i = 0; i < chunkCount; i++) {
+            const outputFile = `${inputFile}_chunk_${i}.mp3`;
+            outputFiles.push(outputFile);
+            yield new Promise((resolve, reject) => {
+                (0, fluent_ffmpeg_1.default)(inputFile)
+                    .setStartTime(i * chunkDuration)
+                    .setDuration(chunkDuration)
+                    .output(outputFile)
+                    .on('end', () => resolve())
+                    .on('error', (err) => reject(err)) // Specify the type of 'err' as 'Error'
+                    .run();
+            });
+        }
+        return outputFiles;
+    });
+}
+bot.command('transcribe', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!ctx.message) {
+        yield ctx.reply('Error: Message is undefined.');
+        return;
+    }
+    const videoUrl = ctx.message.text.split(' ')[1];
+    if (!videoUrl || !ytdl_core_1.default.validateURL(videoUrl)) {
+        yield ctx.reply('Please provide a valid YouTube video URL.');
+        return;
+    }
+    const videoInfo = yield ytdl_core_1.default.getInfo(videoUrl);
+    const videoDuration = parseInt(videoInfo.videoDetails.lengthSeconds);
+    const audioStream = (0, ytdl_core_1.default)(videoUrl, { quality: 'highestaudio', filter: 'audioonly' });
+    const audioFile = `./${videoInfo.videoDetails.videoId}.mp3`;
+    (0, stream_1.pipeline)(audioStream, (0, fs_1.createWriteStream)(audioFile), (err) => __awaiter(void 0, void 0, void 0, function* () {
+        if (err) {
+            yield ctx.reply('Error saving the audio. Please try again.');
+            return;
+        }
+        // Send a message to the user indicating that the bot is processing the request
+        yield ctx.reply('Processing your request. This may take a few minutes, please wait...');
         try {
-            const videoInfo = yield (0, play_dl_1.video_basic_info)(url);
-            console.log('Video info:', videoInfo);
-            const videoDuration = videoInfo.video_details.durationInSec;
-            if (videoDuration < 5400) {
-                const audioStream = yield (0, play_dl_1.stream)(url, { quality: 0 }); // 0 represents the highest audio quality
-                const audioFile = `./${videoInfo.video_details.id}.mp3`;
-                (0, stream_1.pipeline)(audioStream.stream, (0, fs_1.createWriteStream)(audioFile), (err) => __awaiter(void 0, void 0, void 0, function* () {
-                    if (err) {
-                        ctx.reply('Error saving the audio. Please try again.');
-                        return;
-                    }
-                    // Send a message to the user indicating that the bot is processing the request
-                    yield ctx.reply('Processing your request. This may take a few minutes, please wait...');
-                    try {
-                        const transcript = yield (0, whisper_1.transcribe)(audioFile);
-                        const transcriptFile = `./${videoInfo.video_details.id}_transcript.txt`;
-                        yield fs_1.promises.writeFile(transcriptFile, transcript);
-                        yield ctx.replyWithDocument(new grammy_1.InputFile((0, fs_1.createReadStream)(transcriptFile)));
-                        // Clean up the files
-                        (0, fs_1.unlink)(audioFile, () => { });
-                        (0, fs_1.unlink)(transcriptFile, () => { });
-                    }
-                    catch (error) {
-                        ctx.reply('Error transcribing the audio. Please try again.');
-                    }
-                }));
-            }
-            else {
-                ctx.reply('Videos for transcription are limited to 1.5 hours. Please provide a shorter video.');
-            }
+            const chunkDuration = 300; // 5 minutes
+            const audioChunks = yield splitAudioFile(audioFile, videoDuration, chunkDuration);
+            const transcripts = yield Promise.all(audioChunks.map((chunk) => (0, whisper_1.transcribe)(chunk)));
+            const combinedTranscript = transcripts.join('\n');
+            const transcriptFile = `./${videoInfo.videoDetails.videoId}_transcript.txt`;
+            yield fs_1.promises.writeFile(transcriptFile, combinedTranscript);
+            yield ctx.replyWithDocument(new grammy_1.InputFile((0, fs_1.createReadStream)(transcriptFile)));
+            // Clean up the files
+            (0, fs_1.unlink)(audioFile, () => { });
+            (0, fs_1.unlink)(transcriptFile, () => { });
+            audioChunks.forEach((chunk) => (0, fs_1.unlink)(chunk, () => { }));
         }
         catch (error) {
-            console.error('Error fetching the video:', error);
-            ctx.reply('Error fetching the video. Please check the URL and try again.');
+            ctx.reply('Error transcribing the audio. Please try again.');
         }
-    }
+    }));
 }));
-bot.catch((err) => console.error(err));
 bot.start();
--- src/notionizer.ts
+++ src/notionizer.ts
@@ -1,64 +1,87 @@
-// main.ts
 import { Bot, InputFile } from 'grammy';
 import { createWriteStream, createReadStream, promises as fsPromises, unlink } from 'fs';
-import { transcribe } from './whisper'; // Import your Whisper API wrapper here
-import * as dotenv from 'dotenv';
-import { video_basic_info, stream } from 'play-dl';
 import { pipeline } from 'stream';
+import ytdl from 'ytdl-core-discord';
+import ffmpeg from 'fluent-ffmpeg';
+import { transcribe } from './whisper';
+import dotenv from 'dotenv';
 
 dotenv.config();
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const bot = new Bot(process.env.BOT_TOKEN as string);
+const bot = new Bot(BOT_TOKEN!);
 
-bot.command('start', (ctx) => ctx.reply('Welcome! Send me a YouTube URL and I will transcribe the video for you.'));
+async function splitAudioFile(inputFile: string, duration: number, chunkDuration: number): Promise<string[]> {
+  const chunkCount = Math.ceil(duration / chunkDuration);
+  const outputFiles: string[] = []; // Explicitly specify the type as string[]
+
+  for (let i = 0; i < chunkCount; i++) {
+    const outputFile = `${inputFile}_chunk_${i}.mp3`;
+    outputFiles.push(outputFile);
+
+    await new Promise<void>((resolve, reject) => {
+      ffmpeg(inputFile)
+        .setStartTime(i * chunkDuration)
+        .setDuration(chunkDuration)
+        .output(outputFile)
+        .on('end', () => resolve())
+        .on('error', (err: Error) => reject(err))
+        .run();
+    });
+  }
+
+  return outputFiles;
+}
 
 bot.on('message', async (ctx) => {
-  if (ctx.message.text) {
-    const url = ctx.message.text;
-    console.log('URL received:', url);
-
-    try {
-      const videoInfo = await video_basic_info(url);
-      console.log('Video info:', videoInfo);
-      const videoDuration = videoInfo.video_details.durationInSec;
-
-      if (videoDuration < 5400) {
-        const audioStream = await stream(url, { quality: 0 }); // 0 represents the highest audio quality
-        const audioFile = `./${videoInfo.video_details.id}.mp3`;
-      
-        pipeline(audioStream.stream, createWriteStream(audioFile), async (err) => {
-          if (err) {
-            ctx.reply('Error saving the audio. Please try again.');
-            return;
-          }
-        
-          // Send a message to the user indicating that the bot is processing the request
-          await ctx.reply('Processing your request. This may take a few minutes, please wait...');
-        
-          try {
-            const transcript = await transcribe(audioFile);
-            const transcriptFile = `./${videoInfo.video_details.id}_transcript.txt`;
-        
-            await fsPromises.writeFile(transcriptFile, transcript);
-            await ctx.replyWithDocument(new InputFile(createReadStream(transcriptFile)));
-        
-            // Clean up the files
-            unlink(audioFile, () => {});
-            unlink(transcriptFile, () => {});
-          } catch (error) {
-            ctx.reply('Error transcribing the audio. Please try again.');
-          }
-        });
-      } else {
-        ctx.reply('Videos for transcription are limited to 1.5 hours. Please provide a shorter video.');
-      }
-    } catch (error) {
-      console.error('Error fetching the video:', error);
-      ctx.reply('Error fetching the video. Please check the URL and try again.');
+  try {
+    if (!ctx.message || !ctx.message.text) {
+      return; // Ignore non-text messages
     }
+
+    const videoUrl = ctx.message.text;
+
+    if (!videoUrl || !ytdl.validateURL(videoUrl)) {
+      // Do nothing if the message is not a valid YouTube video URL
+      return;
+    }
+
+    const videoInfo = await ytdl.getInfo(videoUrl);
+    const videoDuration = parseInt(videoInfo.videoDetails.lengthSeconds);
+    const audioStream = await ytdl(videoUrl, { quality: 'highestaudio', filter: 'audioonly' });    const audioFile = `./${videoInfo.videoDetails.videoId}.mp3`;
+
+    pipeline(audioStream, createWriteStream(audioFile), async (err) => {
+      if (err) {
+        await ctx.reply('Error saving the audio. Please try again.');
+        return;
+      }
+
+      // Send a message to the user indicating that the bot is processing the request
+      await ctx.reply('Processing your request. This may take a few minutes, please wait...');
+
+      try {
+        const chunkDuration = 300; // 5 minutes
+        const audioChunks = await splitAudioFile(audioFile, videoDuration, chunkDuration);
+        const transcripts = await Promise.all(audioChunks.map((chunk) => transcribe(chunk)));
+
+        const combinedTranscript = transcripts.join('\n');
+        const transcriptFile = `./${videoInfo.videoDetails.videoId}_transcript.txt`;
+
+        await fsPromises.writeFile(transcriptFile, combinedTranscript);
+        await ctx.replyWithDocument(new InputFile(createReadStream(transcriptFile)));
+
+        // Clean up the files
+        unlink(audioFile, () => {});
+        unlink(transcriptFile, () => {});
+        audioChunks.forEach((chunk) => unlink(chunk, () => {}));
+      } catch (error) {
+        ctx.reply('Error transcribing the audio. Please try again.');
+      }
+    });
+  } catch (error) {
+    console.error('Error in the message event handler:', error);
+    await ctx.reply('An error occurred. Please try again.');
   }
 });
-
-bot.catch((err) => console.error(err));
 
 bot.start();
